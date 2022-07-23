03f0d258fafc9dc4394066ab3f84d3bece11ed1d
glyphack <sh.hooshyari@gmail.com>
2022-07-23 18:02:09 +0430

    update fish commands

--- fish/functions/carnext_ssm_hadoop.fish
+++
@@ -1,5 +0,0 @@
-function carnext_ssm_hadoop_session
-    aws --profile datamesh-dev-developer ssm start-session --parameters='command=["sudo -i -u hadoop"]' --document-name AWS-StartInteractiveCommand --target $argv
-end
-
-funcsave carnext_ssm_hadoop_session
