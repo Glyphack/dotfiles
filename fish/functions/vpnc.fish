bb8fd456fe0bd7eca3f25758855152f15f78d815
glyphack <sh.hooshyari@gmail.com>
2022-07-30 14:10:28 +0430

    Add functions to connect/disconnect vpn

---
+++ fish/functions/vpnc.fish
@@ -0,0 +1,3 @@
+function vpnc --wraps='sudo nordvpn c nl' --description 'alias vpnc=sudo nordvpn c nl'
+  sudo nordvpn c nl $argv; 
+end
---
+++ fish/functions/vpnd.fish
@@ -0,0 +1,3 @@
+function vpnd --wraps='sudo nordvpn d' --description 'alias vpnd=sudo nordvpn d'
+  sudo nordvpn d $argv; 
+end
