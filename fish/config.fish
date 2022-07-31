b34d7f3266a703b81371b6ea4755457ba06995e6
glyphack <sh.hooshyari@gmail.com>
2022-07-31 09:43:00 +0430

    Sync with Linux machine

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,10 @@ set -x PYENV_ROOT $HOME/.pyenv
 set -x GOPATH $HOME/go
 set -gx NVM_DIR $HOME/.nvm
 set --universal FZF_DEFAULT_COMMAND fd
+set -x JAVA_HOME $HOME/Library/Caches/Coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz/jdk8u292-b10/Contents/Home
 
-set -x PATH $PATH $PYENV_ROOT/bin $GOPATH/bin $NVM_DIR /Users/glyphack/Library/Python/3.9/bin /Users/glyphack/.local/bin /Users/glyphack/opt/anaconda3/bin /Users/glyphack/opt/anaconda3/condabin /Users/glyphack/.pyenv/shims /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin /usr/local/Cellar/openvpn/2.5.5/sbin
+set -x PATH $PATH $PYENV_ROOT/bin $GOPATH/bin $NVM_DIR $JAVA_HOME /Users/glyphack/Library/Python/3.9/bin /Users/glyphack/.local/bin /Users/glyphack/opt/anaconda3/bin /Users/glyphack/opt/anaconda3/condabin /Users/glyphack/.pyenv/shims /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin /usr/local/Cellar/openvpn/2.5.5/sbin 
+
 
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
