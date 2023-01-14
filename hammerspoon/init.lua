a8bf35d82c0b4d927de944cbe3cbd57462b3b3f3
Glyphack <sh.hooshyari@gmail.com>
2023-01-14 16:04:18 +0100

    Add auto reload

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,15 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+  hs.notify.new({title="Hammerspoon11", informativeText="Hello World"}):send()
 end)
+
+function reload(files)
+  for _, file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      hs.notify.new({title='Reloading', informativeText='Reloading Hammerspoon config'}):send()
+      hs.reload()
+      return
+    end
+  end
+end
+
+reloadWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reload):start()
