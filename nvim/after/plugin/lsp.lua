42531b489daf9f42f3911569a93dfb0024cfb6fd
Glyphack <sh.hooshyari@gmail.com>
2023-02-11 10:14:49 +0100

    Add inline diag message

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,7 +1,6 @@
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
-vim.diagnostic.config({ virtual_text = true })
 
 require('fidget').setup()
 
@@ -19,16 +18,18 @@ lsp.ensure_installed({
     'yamlls',
     'gopls',
     'golangci_lint_ls',
+    'prismals',
+    'marksman',
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-i>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-i>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
 
 -- disable completion with tab
 -- this helps with copilot setup
@@ -39,10 +40,11 @@ lsp.setup_nvim_cmp({
     mapping = cmp_mappings,
     sources = {
         { name = "path" },
-        { name = "nvim_lsp", keyword_length = 1},
-        { name = "buffer", keyword_length = 3 },
-        { name = "luasnip", keyword_length = 2 },
-}})
+        { name = "nvim_lsp", keyword_length = 1 },
+        { name = "buffer",   keyword_length = 3 },
+        { name = "luasnip",  keyword_length = 2 },
+    }
+})
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
@@ -74,6 +76,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, opts)
     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.diagnostic.config({ virtual_text = true })
 end)
 
 local default_schemas = nil
