497a5996baf5a0ecaeeebe40c5ea257edda90924
Glyphack <sh.hooshyari@gmail.com>
2023-03-02 22:16:07 +0100

    Add silent open

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,6 +1,13 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function open_tab_silent(node)
+    local api = require("nvim-tree.api")
+
+    api.node.open.tab(node)
+    vim.cmd.tabprev()
+end
+
 local tree = require("nvim-tree")
 tree.setup({
     actions = {
@@ -8,8 +15,19 @@ tree.setup({
             quit_on_open = false
         }
     },
+    view = {
+        mappings = {
+            list = {
+                { key = "T", action = "open_tab_silent", action_cb = open_tab_silent },
+            },
+        }
+    },
+    filters = { custom = { "^.git" }, git_clean = false, exclude = { ".git", "node_modules" } },
+    render = { git = { enabled = false, ignore = false } },
 }
 )
+
+require("lsp-file-operations").setup()
 
 
 vim.keymap.set("n", "<leader>pe", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
