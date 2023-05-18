a9f6410bee8b04c88131fc5b5c40e98b056b3f25
Glyphack <sh.hooshyari@gmail.com>
2023-05-18 16:02:55 +0200

    save shortcut

--- nvim/lua/glyphack/remap.lua
+++ nvim/lua/glyphack/remap.lua
@@ -41,7 +41,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
+vim.keymap.set("n", "<leader>w", ":w<CR>", opts)
 
 -- Resize with arrows
 vim.keymap.set("n", "<M-Up>", ":resize +2<CR>", opts)
