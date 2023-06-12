d5257fe764d4c2d1165180d69d1a74d0411565c7
Glyphack <sh.hooshyari@gmail.com>
2023-06-12 14:49:57 +0200

    aliases

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,9 @@ set -x PYENV_ROOT $HOME/.pyenv
 set -x POETRY $HOME/.poetry
 set -x GOPATH $HOME/go
 
+# place for software I install
+set -x HOME_BIN $HOME/bin
+
 
 # Python
 set -x VIRTUALFISH_ACTIVATION_FILE .venv
@@ -28,7 +31,7 @@ switch (uname)
         set -x PATH $PATH  /usr/local/opt/fzf/bin /Applications/WezTerm.app/Contents/MacOS
 end
 
-set -x PATH $PATH $PYENV_ROOT/bin $GOPATH/bin $JAVA_HOME $HOME/.local/bin $HOME/.pyenv/shims /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin $POETRY/bin $NPM_PRE
+set -x PATH $PATH $HOME_BIN $PYENV_ROOT/bin $GOPATH/bin $JAVA_HOME $HOME/.local/bin $HOME/.pyenv/shims /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/go/bin $POETRY/bin $NPM_PRE
 
 starship init fish | source
 status is-login; and pyenv init --path | source
@@ -36,3 +39,9 @@ status is-interactive; and pyenv init - | source
 
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+set -x GITHUB_USERNAME shooshyari
+
+alias k="kubectl"
+alias mpr="/Users/shooshyari/Programming/flexport/flexport/mpr"
+alias dev="fx rdev"
