d2919c1156976beb8f3d111513043ef98fc8cb53
Glyphack <sh.hooshyari@gmail.com>
2023-06-19 10:10:22 +0200

    nvm

--- fish/config.fish
+++ fish/config.fish
@@ -47,4 +47,4 @@ status is-interactive; and pyenv init - | source
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 # complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 #
-# load_nvm > /dev/stderr
+nvm use 16
