871ae8a036442a900661fa8c5b9a4ca43745973c
Glyphack <sh.hooshyari@gmail.com>
2023-08-04 22:21:12 +0200

    Goodbye null-ls

--- nvim/lua/glyphack/null-ls.lua
+++ nvim/lua/glyphack/null-ls.lua
@@ -9,68 +9,68 @@ local code_actions = null_ls.builtins.code_actions
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup({
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
-    sources = {
-        formatting.isort.with({
-            extra_args = { "--line-length", "79", "--ca", "--profile", "black", "--float-to-top" },
-        }),
-        formatting.black,
-        -- formatting.scalafmt,
-
-        formatting.goimports,
-        formatting.gofmt,
-        formatting.buf,
-        diagnostics.buf,
-
-        formatting.rustfmt,
-
-        diagnostics.ktlint,
-        -- formatting.ktlint.with({
-        --     args = { "--format", "--stdin", "**/*.kt", "**/*.kts", "--code-style", "ktlint_official" },
-        -- }),
-
-        diagnostics.rubocop.with({
-            command = "~/.rvm/gems/ruby-2.7.7/bin/rubocop",
-        }),
-        formatting.rubocop.with({
-            command = "~/.rvm/gems/ruby-2.7.7/bin/rubocop",
-        }),
-
-        formatting.markdownlint,
-        diagnostics.markdownlint,
-
-        formatting.prettierd.with({
-            filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less",
-                "html", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" }
-        }),
-        formatting.prismafmt,
-        -- diagnostics.eslint_d,
-
-        formatting.shfmt,
-        formatting.packer,
-
-        formatting.sqlfluff.with({
-            extra_args = { "--dialect", "sparksql" }, -- change to your dialect
-        }),
-        diagnostics.sqlfluff.with({
-            extra_args = { "--dialect", "sparksql" }, -- change to your dialect
-        }),
-
-        diagnostics.codespell.with({
-            extra_args = { "--L", "crate" },
-        }),
-        -- formatting.codespell,
-    },
-})
+-- null_ls.setup({
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     vim.lsp.buf.format({ bufnr = bufnr })
+--                 end,
+--             })
+--         end
+--     end,
+--     sources = {
+--         formatting.isort.with({
+--             extra_args = { "--line-length", "79", "--ca", "--profile", "black", "--float-to-top" },
+--         }),
+--         formatting.black,
+--         -- formatting.scalafmt,
+--
+--         formatting.goimports,
+--         formatting.gofmt,
+--         formatting.buf,
+--         diagnostics.buf,
+--
+--         formatting.rustfmt,
+--
+--         diagnostics.ktlint,
+--         -- formatting.ktlint.with({
+--         --     args = { "--format", "--stdin", "**/*.kt", "**/*.kts", "--code-style", "ktlint_official" },
+--         -- }),
+--
+--         diagnostics.rubocop.with({
+--             command = "~/.rvm/gems/ruby-2.7.7/bin/rubocop",
+--         }),
+--         formatting.rubocop.with({
+--             command = "~/.rvm/gems/ruby-2.7.7/bin/rubocop",
+--         }),
+--
+--         formatting.markdownlint,
+--         diagnostics.markdownlint,
+--
+--         formatting.prettierd.with({
+--             filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less",
+--                 "html", "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars" }
+--         }),
+--         formatting.prismafmt,
+--         -- diagnostics.eslint_d,
+--
+--         formatting.shfmt,
+--         formatting.packer,
+--
+--         formatting.sqlfluff.with({
+--             extra_args = { "--dialect", "sparksql" }, -- change to your dialect
+--         }),
+--         diagnostics.sqlfluff.with({
+--             extra_args = { "--dialect", "sparksql" }, -- change to your dialect
+--         }),
+--
+--         diagnostics.codespell.with({
+--             extra_args = { "--L", "crate" },
+--         }),
+--         -- formatting.codespell,
+--     },
+-- })
