7679b197cb3cac69b5635c60e035215dddbbfe9e
Glyphack <sh.hooshyari@gmail.com>
2023-08-04 22:21:28 +0200

    Update unmute text

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,9 +13,8 @@ GlobalMute:configure({
   mute_background       = 'file:///Library/Desktop%20Pictures/Solid%20Colors/Turquoise%20Green.png',
   enforce_desired_state = true,
   stop_sococo_for_zoom  = true,
-  unmute_title          = "<---- THEY CAN HEAR YOU -----",
+  unmute_title          = "<---- UNMUTE -----",
   mute_title            = "<-- MUTE",
-  -- change_screens = "SCREENNAME1, SCREENNAME2"  -- This will only change the background of the specific screens.  string.find()
 })
 spoon.GlobalMute._logger.level = 3
 
