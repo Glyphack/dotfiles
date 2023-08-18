b102722ed2f5ba43310e84ad505b9905d238ece8
Glyphack <sh.hooshyari@gmail.com>
2023-08-18 10:20:26 +0200

    show hidden files

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -24,6 +24,14 @@ vim.g.loaded_netrwPlugin = 1
 -- )
 
 require("lsp-file-operations").setup()
+require("neo-tree").setup({
+    close_if_last_window = true,
+    filesystem = {
+        filtered_items = {
+            visible = true,
+        }
+    },
+})
 
 
 vim.keymap.set("n", "<C-p>", ":Neotree toggle current reveal_force_cwd<cr>", { noremap = true, silent = true })
