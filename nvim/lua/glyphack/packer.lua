3dd508d75c3be3e47fa0da90062c454a9204d7eb
Glyphack <sh.hooshyari@gmail.com>
2023-08-21 21:54:59 +0200

    remove block comment mapping

--- nvim/lua/glyphack/packer.lua
+++ nvim/lua/glyphack/packer.lua
@@ -191,46 +191,49 @@ require("lazy").setup({
         "numToStr/Comment.nvim",
         config = function(plugin)
             require("Comment").setup()
+            -- disable the block comment because I use visuals.
+            vim.api.nvim_del_keymap('n', 'gbc')
         end
     },
     { "nvim-lualine/lualine.nvim" },
     { "github/copilot.vim" },
     { "akinsho/toggleterm.nvim" },
     { "j-hui/fidget.nvim",              tag = "legacy" },
-    {
-        "gaoDean/autolist.nvim",
-        ft = {
-            "markdown",
-            "text",
-            "tex",
-            "plaintex",
-        },
-        config = function()
-            require("autolist").setup()
-
-            vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-            vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-            vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-            vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-            vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-            vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-            vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
-
-            -- cycle list types with dot-repeat
-            vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-            vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
-
-            -- if you don't want dot-repeat
-            -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
-            -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
-
-            -- functions to recalculate list on edit
-            vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
-        end,
-    },
+    -- it's getting annoying when takes control of the cursor and adjusts everything I write
+    -- {
+    --     "gaoDean/autolist.nvim",
+    --     ft = {
+    --         "markdown",
+    --         "text",
+    --         "tex",
+    --         "plaintex",
+    --     },
+    --     config = function()
+    --         require("autolist").setup()
+    --
+    --         vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+    --         vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+    --         vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+    --         vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+    --         vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+    --         vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+    --         vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+    --
+    --         -- cycle list types with dot-repeat
+    --         vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+    --         vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+    --
+    --         -- if you don't want dot-repeat
+    --         -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
+    --         -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+    --
+    --         -- functions to recalculate list on edit
+    --         vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+    --         vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+    --         vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+    --         vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+    --     end,
+    -- },
     {
         "ruifm/gitlinker.nvim",
         dependencies = "nvim-lua/plenary.nvim",
