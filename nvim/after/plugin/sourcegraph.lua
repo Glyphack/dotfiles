89ec455f1f829ea35e01f0b2aa8f3ed1f087b257
Glyphack <sh.hooshyari@gmail.com>
2023-08-27 18:37:43 +0200

    Add sg but disabled for now

---
+++ nvim/after/plugin/sourcegraph.lua
@@ -0,0 +1,2 @@
+-- require("sg").setup {
+-- }
--- nvim/lua/glyphack/packer.lua
+++ nvim/lua/glyphack/packer.lua
@@ -291,5 +291,12 @@ require("lazy").setup({
         config = function()
             require('hlsearch').setup()
         end
-    }
+    },
+    {
+        "sourcegraph/sg.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+
+        -- If you have a recent version of lazy.nvim, you don't need to add this!
+        build = "nvim -l build/init.lua",
+    },
 })
