610e3923988b7c004c00749596e831d8f07ea0f8
Glyphack <sh.hooshyari@gmail.com>
2023-10-15 23:32:16 +0200

    Open file tree and reveal the path in vim

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -16,7 +16,10 @@ vim.keymap.set("n", "<C-p>", ":Neotree toggle current reveal_force_cwd<cr>", { n
 
 require('mini.files').setup()
 local minifiles_toggle = function()
-  if not MiniFiles.close() then MiniFiles.open() end
+  if not MiniFiles.close() then
+        MiniFiles.open(vim.api.nvim_buf_get_name(0))
+        MiniFiles.reveal_cwd()
+    end
 end
 
 vim.keymap.set("n", "<C-t>", minifiles_toggle, { noremap = true, silent = true })
