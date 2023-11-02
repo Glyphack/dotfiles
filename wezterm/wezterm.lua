fc7512eba5593a3e8ee0aa9389bb834a4699d8c6
Glyphack <sh.hooshyari@gmail.com>
2023-11-02 13:54:49 +0100

    Back

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,6 +31,20 @@ local config = {
         act.SendKey { key = 'L', mods = 'CTRL' },
       },
     },
+    {
+      key = "p",
+      mods = "CMD",
+      action = wezterm.action { QuickSelectArgs = {
+        patterns = {
+          "https?://\\S+"
+        },
+        action = wezterm.action_callback(function(window, pane)
+          local url = window:get_selection_text_for_pane(pane)
+          wezterm.log_info("opening: " .. url)
+          wezterm.open_with(url)
+        end)
+      } }
+    },
   },
   color_scheme = "catppuccin-mocha",
 }
