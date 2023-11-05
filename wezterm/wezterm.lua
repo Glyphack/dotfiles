f6a61da2f9f1d2d0d5612fabb1c558f054ce59f2
Glyphack <sh.hooshyari@gmail.com>
2023-11-05 23:36:31 +0100

    Add bg image

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -49,6 +49,32 @@ local config = {
   color_scheme = "catppuccin-mocha",
 }
 
-config.window_background_opacity = .9
+-- config.window_background_opacity = .9
+-- The art is a bit too bright and colorful to be useful as a backdrop
+-- for text, so we're going to dim it down to 10% of its normal brightness
+local dimmer = { brightness = 0.1 }
+
+config.enable_scroll_bar = true
+config.min_scroll_bar_height = '2cell'
+config.colors = {
+  scrollbar_thumb = 'gray',
+}
+config.background = {
+  -- This is the deepest/back-most layer. It will be rendered first
+  {
+    source = {
+      File = wezterm.home_dir .. '/Programming/dotfiles/bg-1.jpeg',
+    },
+    -- The texture tiles vertically but not horizontally.
+    -- When we repeat it, mirror it so that it appears "more seamless".
+    -- An alternative to this is to set `width = "100%"` and have
+    -- it stretch across the display
+    repeat_x = 'Mirror',
+    hsb = dimmer,
+    -- When the viewport scrolls, move this layer 10% of the number of
+    -- pixels moved by the main viewport. This makes it appear to be
+    -- further behind the text.
+  },
+}
 
 return config
