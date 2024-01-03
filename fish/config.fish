8eb083cffd4f05bc46dc8327d9d64755d747de5d
Glyphack <sh.hooshyari@gmail.com>
2024-01-03 20:53:05 +0100

    Conda in fish

--- fish/config.fish
+++ fish/config.fish
@@ -68,3 +68,16 @@ end
 # pnpm end
 
  source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f ~/miniconda3/bin/conda
+    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+        . "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "$HOME/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
