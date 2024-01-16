89327474adf8e74e5f9dc2abd03d8a2d0111580c
Glyphack <sh.hooshyari@gmail.com>
2024-01-17 00:14:41 +0100

    Updates

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,25 +1,10 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require("lsp-file-operations").setup()
-require("neo-tree").setup({
-    close_if_last_window = true,
-    filesystem = {
-        filtered_items = {
-            visible = true,
-        }
-    },
-})
-
-
-vim.keymap.set("n", "<C-p>", ":Neotree toggle current reveal_force_cwd<cr>", { noremap = true, silent = true })
-
-require('mini.files').setup()
+require("mini.files").setup()
 local minifiles_toggle = function()
-  if not MiniFiles.close() then
-        MiniFiles.open(vim.api.nvim_buf_get_name(0))
-        MiniFiles.reveal_cwd()
-    end
+	if not MiniFiles.close() then
+		MiniFiles.open(vim.api.nvim_buf_get_name(0))
+		MiniFiles.reveal_cwd()
+	end
 end
 
 vim.keymap.set("n", "<C-t>", minifiles_toggle, { noremap = true, silent = true })
--- nvim/lua/glyphack/packer.lua
+++ nvim/lua/glyphack/packer.lua
@@ -178,16 +178,6 @@ require("lazy").setup({
 	{ "lewis6991/gitsigns.nvim" },
 	{ "andweeb/presence.nvim" },
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	{ "jose-elias-alvarez/null-ls.nvim" },
-	{
 		"numToStr/Comment.nvim",
 		config = function()
 			require("Comment").setup({
