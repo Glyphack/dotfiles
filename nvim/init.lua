eb3da6e2472959776666f572eee02acad184c6fb
Glyphack <sh.hooshyari@gmail.com>
2024-10-19 15:50:23 +0200

    Rust disable check on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -677,18 +677,18 @@ require("lazy").setup({
 		version = "^5",
 		lazy = false,
 		config = function()
-			-- local mason_registry = require("mason-registry")
-			-- local codelldb = mason_registry.get_package("codelldb")
-			-- local extension_path = codelldb:get_install_path() .. "/extension/"
-			-- local codelldb_path = extension_path .. "adapter/codelldb"
-			-- local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			-- local cfg = require("rustaceanvim.config")
-
-			-- vim.g.rustaceanvim = {
-			-- 	dap = {
-			-- 		adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-			-- 	},
-			-- }
+			vim.g.rustaceanvim = {
+				tools = {},
+				server = {
+					default_settings = {
+						["rust-analyzer"] = {
+							checkOnSave = {
+								enabled = false,
+							},
+						},
+					},
+				},
+			}
 		end,
 	},
 	-- {
