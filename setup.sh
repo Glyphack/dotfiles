8eb975df0aa5e06c483afa4be9550b675b30fa64
Glyphack <sh.hooshyari@gmail.com>
2026-06-27 13:00:34 +0200

    Cleanup

--- setup.sh
+++ setup.sh
@@ -9,19 +9,10 @@ echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 chsh -s /opt/homebrew/bin/fish
 
 
-echo "Languages setup"
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-mkdir -p ~/.virtualenvs/
-
 mise i
 
 echo "settings"
 sh ./setup/block-sites.sh
 sh ./setup/mac-settings.sh
-
-echo "tools"
-
-playwright install --with-deps chromium
 
 fish
